c05d50561fca5c32ce5dc5fbad2fca2cb59612d0
HericMendez <heric.mendez00@gmail.com>
2022-08-09 10:12:23 -0300

    dados da api transferidos para o front-end

---
+++ assets/scripts/generator.js
@@ -0,0 +1,40 @@
+const tituloFilme = () => {
+  const mod = document.getElementById("mod").value;
+  const cat = document.getElementById("categoria").value;
+
+  let arr = "";
+  switch (cat) {
+    case "filmes":
+      arr = titulos.filmes;
+
+      break;
+    case "series":
+      arr = titulos.series;
+      break;
+  }
+  if (arr != "") {
+    const randomMovie = arr[Math.floor(Math.random() * arr.length)];
+
+    const myArr = randomMovie.split(" ");
+    let rng = "";
+    while (rng.length < 4) {
+      rng = myArr[Math.floor(Math.random() * myArr.length)];
+    }
+
+    const joined = myArr.join(" ");
+
+    const newMovie = joined.replace(rng, mod);
+
+    const newObj = {
+      categoria: cat,
+      original: randomMovie,
+      alterado: newMovie,
+    };
+
+    document.getElementById("original").innerHTML = newObj.original;
+    document.getElementById("alterado").innerHTML = newObj.alterado;
+    console.log(newObj)
+  } else {
+    return { erro: "Selecione uma categoria!" };
+  }
+};
